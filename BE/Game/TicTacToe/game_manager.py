e740022b06c247e5bf4239247dece5bc890b7fe1
jmpumuro <jmpumuro@gmail.com>
2023-09-13 21:23:55 -0600

    Revert "config file added"
    This reverts commit 4d222fc9c94dfc66cfc2f654e2b58bbb7a692b1e.

--- BE/Game/TicTacToe/game_manager.py
+++ BE/Game/TicTacToe/game_manager.py
@@ -1,17 +1,15 @@
 from .game import TicTacToe
 from .agent import TicTacToeQLearning
-import json
 
 class GameManager:
     _instance = None
-    def __new__(cls,config_path='/Users/joempumuro/TicTacToe-Q-Learner-Interactive/BE/config.json'):
+
+    def __new__(cls):
         if not cls._instance:
             cls._instance = super(GameManager, cls).__new__(cls)
             cls._instance.board = TicTacToe()
             cls._instance.q_learning = TicTacToeQLearning()
             cls._instance.is_agent_turn = True
-            with open(config_path, 'r') as config_file:
-                cls._instance.config = json.load(config_file)
         return cls._instance
 
     def get_board_state(self):
@@ -29,8 +27,7 @@ class GameManager:
     def make_agent_move(self, board,q_learning):
         if self.is_agent_turn:
             # Load the Q-values from a file
-            model_path = self.config['model_path']
-            q_learning.load_model(model_path)
+            q_learning.load_model('/Users/joempumuro/TicTacToe/BE/Game/TicTacToe/model.keras')
 
             # Get the current game state
             state = board.get_state()
--- BE/Game/TicTacToe/views.py
+++ BE/Game/TicTacToe/views.py
@@ -62,15 +62,12 @@ class TrainingState:
 
 
 class TrainAgentView(GameBoardMixin, APIView):
-    
-    def __init__(self,config_path='/Users/joempumuro/TicTacToe-Q-Learner-Interactive/BE/config.json'):
+    def __init__(self):
         super().__init__()
         self.training_state = TrainingState.NOT_STARTED
         self.stop_training = threading.Event()
         self.training_state_lock = threading.Lock()
         self.training_thread = None
-        with open(config_path, 'r') as config_file:
-            self.config = json.load(config_file)
 
     def get_training_state(self):
         with self.training_state_lock:
@@ -96,10 +93,9 @@ class TrainAgentView(GameBoardMixin, APIView):
 
             # Train the agent
             q_learning.train(episodes,epsilon,gamma)
-    
+
             # Save the trained Q-values
-            model_path = self.config['model_path']
-            q_learning.save_model(model_path)  # Update the path accordingly
+            q_learning.save_model('/Users/joempumuro/TicTacToe/BE/Game/TicTacToe/model')  # Update the path accordingly
 
             self.set_training_state(TrainingState.COMPLETE)
         except json.JSONDecodeError as e:
